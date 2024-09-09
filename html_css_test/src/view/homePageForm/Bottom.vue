40fdcb40f3265247d8a470428561954af8679060
pourry <1591311356@qq.com>
2024-09-09 22:37:54 +0800

    20240909

--- html_css_test/src/view/homePageForm/Bottom.vue
+++ html_css_test/src/view/homePageForm/Bottom.vue
@@ -1,5 +1,6 @@
 <template>
     <div> 
+      <div>底部</div>
     </div>
 </template>
 
--- html_css_test/src/view/router/index.js
+++ html_css_test/src/view/router/index.js
@@ -22,13 +22,13 @@ const routes = [
          //登录页面
          path: '/userSelf',
          name: 'UserSelf',
-         component: UserSelf,
+         component: UserSelf
       },
       {
          //登录页面
          path: '/login',
          name: 'Login',
-         component: Login,
+         component: Login
       },
     ]
  },
